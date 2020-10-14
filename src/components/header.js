d789e0681a9186add0470a04fd1b8ed5859b9376
Andrew Price <andrewp@ap-mac.broadband>
2020-10-14 15:19:59 +0100

    fonts

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,13 @@ import {NavLink} from 'react-router-dom';
 
 const Header = () => {
     return(
-        <div className="banner-header">
-           
-            <div className="top-navi">
+        <div className="top-navi">
                 
                 <NavLink to="/" className="item ">Home</NavLink>
                 <NavLink to="/process" className="item">Process</NavLink>
                 <NavLink to="/projects" className="item">Projects</NavLink>
             </div>
-        </div>
+        
     );
 }
 
--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import './styles/home.css'
 import Profile from './styles/assets/profile-img.png';
-import pmCert from './styles/assets/pm-cert.png';
-import smCert from './styles/assets/sm-cert.png';
+import {NavLink} from 'react-router-dom';
 
-import AzureRP from './styles/assets/azure-rp.png';
-import XD from './styles/assets/xd-icon.png';
-import Figma from './styles/assets/figma.png';
-import Sketch from './styles/assets/sketch.png';
 
 
 const home = () => {
     return(
         <div className="home-content">
             <div className="heading-container">
+                
+                <div className="title-container">
+                    <h1>Andy Price</h1>
+                    <h3>Product Design Leader</h3>
+                    
+                    <NavLink to="/projects" className="item">
+                        <div className="primary-button">Projects</div>
+                    </NavLink>
+                    
+                </div>
                 <div className="image-container">
                     <img className="profile-image" src={Profile} alt="hero" />
                 </div>
-                <div className="title-container">
-                    <h1>Andy Price</h1>
-                </div>
+
+
                 <div className="intro-container">
-                    <p>
+                   
+                </div>
+                
+            </div>
+           
+           
+            <div className="overview-container intro brown white-text">
+                <div className="title-box">
+                    
+                    <h2>About Me</h2>
+                </div>
+                <p>
                     Hi, I am a product design leader and tech enthusiast based in rural Nottinghamshire.
                     </p>
                     <p>
@@ -32,36 +47,6 @@ const home = () => {
                     <p>
                     My passion for UX has allowed me tackle a wide range of design projects and nurture design teams, whilst embedding alignment with Product & Development functions. 
                     </p>
-                </div>
-                
-            </div>
-            <div className="icon-container">
-                    <div className="left-logos">
-                        <img className="icon pmCert" src={pmCert} alt="hero" />
-                        <img className="icon smCert" src={smCert} alt="hero" />
-                    </div>
-                    <div className="right-logos">
-                        <img className="icon Sketch" src={Sketch} alt="hero" /> 
-                        <img className="icon Figma" src={Figma} alt="hero" />
-                        <img className="icon XD" src={XD} alt="hero" />
-                        <img className="icon AzureRP" src={AzureRP} alt="hero" /> 
-                    </div>
-                   
-                </div>
-            
-            
-           
-            <div className="overview-container intro yellow">
-                <div className="title-box">
-                    
-                    <h2>Overview</h2>
-                </div>
-                <p>
-                I have developed my skillset over my career to allow me to use experiences from my past to help me tackle design challenges. 
-                A design challenge can vary from a product or project, to gaining buy in for the values of UX or a specific approach. 
-                I follow a standard approach towards all projects that is an evolution of Design Thinking streamlined to allow me to work at 
-                speed where required. My teams have successfully adopted this process with my focus being on visibility of progress & clear achievable milestones.
-                </p>
                 
             </div>
 
